0b4b615df21a1ea25fc50ff8bf1fdd3b87bdf344
ZiTe <honmonoh@gmail.com>
2025-04-03 21:32:51 +0800

    fix: app table x scroll

--- src/components/AppTable.tsx
+++ src/components/AppTable.tsx
@@ -3,7 +3,7 @@ import { DataTable } from "@/components/DataTable";
 
 export default function AppTable({ data }: { data: App[] }) {
   return (
-    <div className="container mx-auto py-4 overflow-x-scroll">
+    <div className="container mx-auto py-4 overflow-x-auto">
       <DataTable columns={columns} data={data} />
     </div>
   );
