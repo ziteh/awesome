47eb9cfe6d87753733218088d44a3cdf01490845
ZiTe <honmonoh@gmail.com>
2025-04-03 21:42:52 +0800

    chore: update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://awesome.ziteh.dev",
+
   vite: {
     plugins: [tailwindcss()],
   },
